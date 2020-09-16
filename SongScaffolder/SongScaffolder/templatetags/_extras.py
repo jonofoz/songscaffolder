de84866f504ab4b82496963eca5cb44c0e1e5796
Werewoof <lorddrawcula@gmail.com>
2020-09-16 11:36:12 -0500

    Add new 'replace_dashes' template tag

--- SongScaffolder/SongScaffolder/templatetags/_extras.py
+++ SongScaffolder/SongScaffolder/templatetags/_extras.py
@@ -4,7 +4,13 @@ register = template.Library()
 
 @register.filter(name="addstr")
 def addstr(arg1, arg2):
-    """concatenate arg1 & arg2"""
+    """Returns a concatenation of arg 1 and arg2, after replacing problematic characters in both."""
     arg1 = str(arg1).replace("/", "-over-").replace(" ", "-space-")
     arg2 = str(arg2).replace("/", "-over-").replace(" ", "-space-")
     return arg1 + arg2
+
+
+@register.filter(name="replace_dashes")
+def replace_dashes(arg1, arg2):
+    """Returns a string with any dashes replaced by spaces."""
+    return str(arg2).replace("-", " ")
