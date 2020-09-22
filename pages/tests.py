9442cb9465e4e627b7916da87763bb3aaaa9b8be
Werewoof <lorddrawcula@gmail.com>
2020-09-21 21:32:58 -0500

    #002 add 'test_signup'

--- pages/tests.py
+++ pages/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.test.utils import tag
 
 ss_test_user_name = "SongScaffolderTestUser"
-ss_test_user_pass = "4CKke4NXzWKsaKU"
+ss_test_user_pass = "IsThi$Pa$$w0rdGoodEnough4Ye"
 # Create your tests here.
 
 class UserTestCase(TestCase):
@@ -13,8 +14,21 @@ class UserTestCase(TestCase):
 
         self.client = Client()
 
+    @tag('skip_setup')
     def test_signup(self):
-        pass
+        # GET
+        response = self.client.get(reverse("pages:signup"))
+        self.assertEqual(response.resolver_match.func.__name__, "user_signup")
+        self.assertEqual(response.status_code, 200)
+        # POST
+        response = self.client.post(reverse("pages:signup"), {"username": ss_test_user_name, "password1": ss_test_user_pass, "password2": ss_test_user_pass, "use_test_db":True}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.redirect_chain, [])
+        self.assertEqual(response.context["user"].username, '')
+        self.assertTrue("password" not in response.context)
+        content_decoded = response.content.decode("utf-8")
+        self.assertFalse(f"(Hi, {ss_test_user_name}!)" in content_decoded)
+        self.assertTrue("<title>\n        \nSign Up\n\n    </title>" in content_decoded)
 
 
     def test_login(self):
