9906e2378f74fb42e6076d480f0c2def698e6d9a
Werewoof <lorddrawcula@gmail.com>
2020-09-28 16:20:36 -0500

    #5 Improve config page styling on mobile devices

--- SongScaffolder/templatetags/_extras.py
+++ SongScaffolder/templatetags/_extras.py
@@ -9,8 +9,12 @@ def addstr(arg1, arg2):
     arg2 = str(arg2).replace("/", "-over-").replace(" ", "-space-")
     return arg1 + arg2
 
-
 @register.filter(name="replace_dashes")
 def replace_dashes(arg1, arg2):
     """Returns a string with any dashes replaced by spaces."""
     return str(arg2).replace("-", " ")
+
+@register.filter(name="subtract_one")
+def subtract_one(arg1, arg2):
+    """Converts a string number into an int, subtracts one, then returns the result as a string."""
+    return str(arg1)+str(int(arg2)-1)
