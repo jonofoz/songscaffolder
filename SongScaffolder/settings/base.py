27853ae0ed59dc121d50c53b611d98b06e6c6629
Werewoof <lorddrawcula@gmail.com>
2020-10-06 08:36:27 -0500

    #11 Set DEBUG to False

--- SongScaffolder/settings/base.py
+++ SongScaffolder/settings/base.py
@@ -32,7 +32,7 @@ except KeyError:
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1']
 
