c1d386047c283decf3d7b6796065321aee3ea7f4
Werewoof <lorddrawcula@gmail.com>
2020-10-07 10:50:54 -0500

    #9 Edit .travis.yml and settings to attempt fixing Travis tests

--- SongScaffolder/settings.py
+++ SongScaffolder/settings.py
@@ -104,8 +104,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASS"),
-        'HOST': os.getenv("DB_HOST"),
+        'PASSWORD': os.getenv("DB_PASS", ""),
+        'HOST': os.getenv("DB_HOST", ""),
         'PORT': os.getenv("DB_PORT"),
     }
 }
